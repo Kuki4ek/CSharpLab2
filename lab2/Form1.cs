8c5bc2639505189c4c2648f35eb897b04cd0659a
Kuki <kuki4ek@yandex.ru>
2026-01-02 08:45:09 +0300

    Добавьте файлы проекта.

---
+++ lab2/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            int [] time1_1 = {dateTimePicker1.Value.Hour, dateTimePicker1.Value.Minute, dateTimePicker1.Value.Second };
+            int [] time1_2 = { dateTimePicker2.Value.Hour, dateTimePicker2.Value.Minute, dateTimePicker2.Value.Second };
+            Time Time1_1 = new Time(time1_1[0], time1_1[1], time1_1[2]);
+            Time Time1_2 = new Time(time1_2[0], time1_2[1], time1_2[2]);
+            Time Time1_3 = new Time();
+            if (comboBox1.Text == "+")
+            {
+                Time1_3 = Time1_1 + Time1_2;
+            }
+            else
+            {
+                Time1_3 = Time1_1 - Time1_2;
+            }
+            textBox1.Text = Time1_3.day.ToString();
+            textBox2.Text = Time1_3.hour.ToString();
+            textBox3.Text = Time1_3.minute.ToString();
+            textBox4.Text = Time1_3.second.ToString();
+            //-----------------------------------------------
+            int[] time2_1 = { int.Parse(textBox12.Text), int.Parse(textBox11.Text), int.Parse(textBox10.Text), int.Parse(textBox9.Text) };
+            int[] time2_2 = { int.Parse(textBox16.Text), int.Parse(textBox15.Text), int.Parse(textBox14.Text), int.Parse(textBox13.Text) };
+            Time Time2_1 = new Time(time2_1[0], time2_1[1], time2_1[2], time2_1[3]);
+            Time Time2_2 = new Time(time2_2[0], time2_2[1], time2_2[2], time2_2[3]);
+            Time Time2_3 = new Time();
+            if (comboBox2.Text == "+")
+            {
+                Time2_3 = Time2_1 + Time2_2;
+            }
+            else
+            {
+                Time2_3 = Time2_1 - Time2_2;
+            }
+            textBox8.Text = Time2_3.day.ToString();
+            textBox7.Text = Time2_3.hour.ToString();
+            textBox6.Text = Time2_3.minute.ToString();
+            textBox5.Text = Time2_3.second.ToString();
+        }
+    }
+}
---
+++ lab2/Time.cs
@@ -0,0 +1,158 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class Time
+    {
+        public int day { get; set; }
+        public int hour { get; set; }
+        public int minute { get; set; }
+        public int second { get; set; }
+
+        public Time()
+        {
+            this.day = 0;
+            this.hour = 0;
+            this.minute = 0;
+            this.second = 0;
+        }
+        public Time(int second)
+        {
+            const int SecondsInMinute = 60;
+            const int SecondsInHour = 60 * 60;
+            const int SecondsInDay = 24 * 60 * 60;
+
+            this.second = second % SecondsInMinute;
+            this.minute = (second / SecondsInMinute) % 60;
+            this.hour = (second / SecondsInHour) % 24;
+            this.day = second / SecondsInDay;
+        }
+        public Time(int minute, int second)
+        {
+            const int SecondsInMinute = 60;
+            const int SecondsInHour = 60 * 60;
+            const int SecondsInDay = 24 * 60 * 60;
+
+            second = second + minute * SecondsInMinute;
+
+            this.second = second % SecondsInMinute;
+            this.minute = (second / SecondsInMinute) % 60;
+            this.hour = (second / SecondsInHour) % 24;
+            this.day = second / SecondsInDay;
+        }
+        public Time(int hour, int minute, int second)
+        {
+            const int SecondsInMinute = 60;
+            const int SecondsInHour = 60 * 60;
+            const int SecondsInDay = 24 * 60 * 60;
+
+            second = second + minute * SecondsInMinute + hour * SecondsInHour;
+
+            this.second = second % SecondsInMinute;
+            this.minute = (second / SecondsInMinute) % 60;
+            this.hour = (second / SecondsInHour) % 24;
+            this.day = second / SecondsInDay;
+        }
+        public Time(int day, int hour, int minute, int second)
+        {
+            const int SecondsInMinute = 60;
+            const int SecondsInHour = 60 * 60;
+            const int SecondsInDay = 24 * 60 * 60;
+
+            second = second + minute * SecondsInMinute + hour * SecondsInHour + day * SecondsInDay;
+
+            this.second = second % SecondsInMinute;
+            this.minute = (second / SecondsInMinute) % 60;
+            this.hour = (second / SecondsInHour) % 24;
+            this.day = second / SecondsInDay;
+        }
+        public int GetFullSecond()  // Возвращает полное коллисчество секунд
+        {
+            return second + 60 * GetFullMinute();
+        }
+        public int GetFullMinute()
+        {
+            return minute + 60 * GetFullHour();
+        }
+        public int GetFullHour()
+        {
+            return hour + 24 * GetFullDay();
+        }
+        public int GetFullDay()
+        {
+            return day;
+        }
+        public string SecondToString()  // Переводит секунды в специальный формат SS
+        {
+            string result = "";
+            if (second > 9)
+            {
+                result = $"{second}";
+            }
+            else
+            {
+                result = $"0{second}";
+            }
+            return result;
+        }
+        public string MinuteToString()  // Переводит минуты в специальный формат MM
+        {
+            string result = "";
+            if (minute > 9)
+            {
+                result = $"{minute}";
+            }
+            else
+            {
+                result = $"0{minute}";
+            }
+            return result;
+        }
+        public string HourToString()  // Переводит часы в специальный формат HH
+        {
+            string result = "";
+            if (hour > 9)
+            {
+                result = $"{hour}";
+            }
+            else
+            {
+                result = $"0{hour}";
+            }
+            return result;
+        }
+        public string DayToString()  // Переводит дни в специальный формат: D
+        {
+            string result = $"{day}";
+            return result;
+        }
+        public override string ToString() // Возвращает промежуток времени в формате Dd HH:MM:SS
+        {
+            return $"{DayToString()}d {HourToString()}:{MinuteToString()}:{SecondToString()}";
+        }
+        public static Time operator +(Time a, Time b)
+        {
+            int sum_sec = a.GetFullSecond() + b.GetFullSecond();
+           
+            Time c = new Time(sum_sec);
+            return c;
+        }
+        public static Time operator -(Time a, Time b)
+        {
+            int sum_sec = a.GetFullSecond() - b.GetFullSecond();
+            Time c = new Time(sum_sec);
+            return c;
+        }
+        public static Time operator *(Time a, int b)
+        {
+            int sum_sec = a.GetFullSecond() * b;
+            Time c = new Time(sum_sec);
+            return c;
+        }
+    }
+}
